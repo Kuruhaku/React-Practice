3c2efb39caa7f2d820e9f25693437e1ce77c81b7
KuruKuru <teaapplesauce35@gmail.com>
2026-05-28 15:56:49 +0800

    Meme generator complete: learn useEffect and how its work

--- meme-generator/components/Content.jsx
+++ meme-generator/components/Content.jsx
@@ -1,29 +1,69 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Content() {
   const [meme, setMeme] = useState({
-    topText: "Test",
-    bottomText: "Test",
+    topText: "One does not simply",
+    bottomText: "Walk into Modor",
     imageUrl: "http://i.imgflip.com/1bij.jpg",
   });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    console.log("rendered");
+    fetch(`https://api.imgflip.com/get_memes`)
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * allMemes.length);
+    const randomImage = allMemes[randomIndex].url;
+    console.log(randomImage);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      imageUrl: randomImage,
+    }));
+  }
+
+  function handleChange(event) {
+    console.log(event.currentTarget);
+    const { name, value } = event.currentTarget;
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
 
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+          />
         </label>
 
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getRandomImage}>Get a new meme image</button>
       </div>
 
       <div className="meme">
+        <span>{meme.imageUrl}</span>
         <img src={meme.imageUrl} />
         <span className="top">{meme.topText}</span>
         <span className="bottom">{meme.bottomText}</span>
